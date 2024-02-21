9e19b15ea3eafe31e175cbdc3238990dcc73ce5a
Aldee <aldee.cariaso@gmail.com>
2024-02-21 14:40:46 -0700

    created base timer circle and style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Header from '../src/components/Header/Header'
+import Header from '../src/components/Header/Header';
+import SubTimer from './components/SubTimer/SubTimer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <SubTimer></SubTimer>
     </div>
   );
 }
---
+++ src/components/SubTimer/SubTimer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import '../SubTimer/SubTimer.scss';
+
+const SubTimer = () => {
+
+    return (
+
+        <div className="timer">
+             
+             <div className="timer__container"> 
+                <div className="circle"></div>
+                <div className="circle--1"></div>
+                <div className="circle--2"></div>
+             </div>
+            
+        </div>
+    )
+}
+
+export default SubTimer;
