39b865ce0f8b0dc3c97e9724428bf617d0fef83d
Aldee <aldee.cariaso@gmail.com>
2024-02-21 16:32:07 -0700

    added new icons, changed icon position

--- src/components/HomeHub/HomeHub.jsx
+++ src/components/HomeHub/HomeHub.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import '../HomeHub/HomeHub.scss';
+import iconHome from '../../assets/icons/icon-timer.png';
+import iconCommunity from '../../assets/icons/icon-community2.png';
+import iconActivity from '../../assets/icons/icon-activity2.png';
+import iconProfile from '../../assets/icons/icon-user2.png';
+import iconFood from '../../assets/icons/icon-foodNutrition.png'
 
 
 const HomeHub = () => {
@@ -7,10 +12,12 @@ const HomeHub = () => {
     return (
 
        <nav className="homeNav">
-            <img className="nav" src=""></img>
-            <img className="nav" src=""></img>
-            <img className="nav" src=""></img>
-            <img className="nav" src=""></img>
+            <img className="nav" src={iconHome}></img>
+            <img className="nav" src={iconActivity}></img>
+            <img className="nav" src={iconFood}></img>
+            <img className="nav" src={iconCommunity}></img>
+            <img className="nav" src={iconProfile}></img>
+            
        </nav>
     )
 }
