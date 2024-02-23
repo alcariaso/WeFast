bd5c7db2ef2c9c5e864c0264533cf6240d800883
Aldee <aldee.cariaso@gmail.com>
2024-02-23 00:15:11 -0700

    initial journal html structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from '../src/components/Header/Header'
 import './App.css';
 import HomeHub from './components/HomeHub/HomeHub';
+import Journal from './components/Journal/Journal';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Journal></Journal>
       <HomeHub></HomeHub>
     </div>
   );
--- src/components/Journal/Journal.jsx
+++ src/components/Journal/Journal.jsx
@@ -5,9 +5,29 @@ const Journal = () => {
 
     return (
 
-        <>
-        hello
-        </>
+        <main className="journal">
+            <div className="task">
+                <h2 className="task__title">MOOD</h2>
+                <p className="task__text"></p>
+            </div>
+            <div className="task">
+                <h2 className="task__title">WATER</h2>
+                <p className="task__text"></p>
+            </div>
+            <div className="task">
+                <h2 className="task__title">MEDICINE</h2>
+                <p className="task__text"></p>
+            </div>
+            <div className="task">
+                <h2 className="task__title">ACTIVITY</h2>
+                <p className="task__text"></p>
+            </div>
+            <div className="task">
+                <h2 className="task__title">BLOOD SUGAR</h2>
+                <p className="task__text"></p>
+            </div>
+        </main>
+
     )
 }
 
