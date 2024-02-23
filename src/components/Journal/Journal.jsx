3d6c4b2dff37a2ee9adcd6800ba57d47685f992a
Aldee <aldee.cariaso@gmail.com>
2024-02-22 23:43:28 -0700

    initial feature Journal set-up

---
+++ src/components/Journal/Journal.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../Journal/Journal.scss';
+
+const Journal = () => {
+
+    return (
+
+        <>
+        hello
+        </>
+    )
+}
+
+export default Journal;
