a9c7f54895e2ee59cc06629482c32e63c9a27eab
Aldee <aldee.cariaso@gmail.com>
2024-02-23 13:13:31 -0700

    initial commit,

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from '../src/components/Header/Header'
 import './App.css';
 import HomeHub from './components/HomeHub/HomeHub';
+import Community from '../src/components/Community/Community';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Community></Community>
       <HomeHub></HomeHub>
     </div>
   );
---
+++ src/components/Community/Community.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../Community/Community.scss';
+
+const Community = () => {
+
+    return (
+
+        <></>
+    )
+}
+
+export default Community
