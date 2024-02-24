b1f8a7474d3d9f471b6e778faa7397a73a7c201f
Aldee <aldee.cariaso@gmail.com>
2024-02-24 11:22:19 -0700

    added styles and images, initial set-up

--- src/components/Community/Community.jsx
+++ src/components/Community/Community.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import '../Community/Community.scss';
+import iconAdd from'../../assets/icons/icon-add.png';
+import Heart from "@react-sandbox/heart";
+import foodImg from '../../assets/images/food.jpg'
+import { useState } from "react";
+import profileImg from '../../assets/images/priyanka-chopra.jpg';
+import sucessGif from '../../assets/images/firework.gif';
+import weighttGif from '../../assets/images/weight-scale.gif'
 
 const Community = () => {
+    const [active, setActive] = useState(false)
 
     return (
 
@@ -9,20 +17,81 @@ const Community = () => {
 
             <div className="feed">
 
-                <div className="feed__Top">
+                <div className="feed__top">
                     <div className="profile">
-                        <img className="profile__img"></img>
-                        <h3 className="profile__title"></h3>
+                        <img className="profile__img" src={profileImg}></img>
+                        <h3 className="profile__title">Priyanka Chopra</h3>
                     </div>
+                    <p className="count">4 Days Fasting</p>
                 </div>
 
-                <div className="feed__Main">
-                    <img className="body"></img>
+                <div className="feed__main">
+                    <img className="body" src={foodImg}></img>
                 </div>
 
-                <div className="feed__Bottom">
-                    <img className="resBtn" src=""></img>
-                    <img className="resBtn" src=""></img>
+                <div className="feed__bottom">
+                    {/* <img className="resBtn" src={iconHeart}></img> */}
+                    <Heart
+                    inactiveColor="#2f3131"
+                    strokeWidth={50}
+                        width={34}
+                        height={34}
+                        active={active}
+                        onClick={() => setActive(!active)}
+                    />
+                    <img className="resBtn" src={iconAdd}></img>
+                </div>
+
+                <div className="feed__top">
+                    <div className="profile">
+                        <img className="profile__img" src={profileImg}></img>
+                        <h3 className="profile__title">Priyanka Chopra</h3>
+                    </div>
+                    <p className="count">4 Days Fasting</p>
+                </div>
+
+                <div className="feed__main">
+                    <p className="text">Week 1 <span className="text--isCompleted">Goal Reached!</span></p>
+                    <img className="body--img" src={sucessGif}></img>
+                </div>
+
+                <div className="feed__bottom">
+                    {/* <img className="resBtn" src={iconHeart}></img> */}
+                    <Heart
+                    inactiveColor="#2f3131"
+                    strokeWidth={50}
+                        width={34}
+                        height={34}
+                        active={active}
+                        onClick={() => setActive(!active)}
+                    />
+                    <img className="resBtn" src={iconAdd}></img>
+                </div>
+
+                <div className="feed__top">
+                    <div className="profile">
+                        <img className="profile__img" src={profileImg}></img>
+                        <h3 className="profile__title">Priyanka Chopra</h3>
+                    </div>
+                    <p className="count">4 Days Fasting</p>
+                </div>
+
+                <div className="feed__main">
+                    <p className="text">Week 2 Weight Loss <span className="text--isCompleted">3 lbs!</span></p>
+                    <img className="body--img" src={weighttGif}></img>
+                </div>
+
+                <div className="feed__bottom">
+                    {/* <img className="resBtn" src={iconHeart}></img> */}
+                    <Heart
+                    inactiveColor="#2f3131"
+                    strokeWidth={50}
+                        width={34}
+                        height={34}
+                        active={active}
+                        onClick={() => setActive(!active)}
+                    />
+                    <img className="resBtn" src={iconAdd}></img>
                 </div>
 
             </div>
