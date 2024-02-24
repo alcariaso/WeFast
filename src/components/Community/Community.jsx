7b064390962a658187f7421a63577eee0a621ffc
Aldee <aldee.cariaso@gmail.com>
2024-02-24 08:22:45 -0700

    initial component structure

--- src/components/Community/Community.jsx
+++ src/components/Community/Community.jsx
@@ -5,8 +5,30 @@ const Community = () => {
 
     return (
 
-        <></>
-    )
+        <main className="community">
+
+            <div className="feed">
+
+                <div className="feed__Top">
+                    <div className="profile">
+                        <img className="profile__img"></img>
+                        <h3 className="profile__title"></h3>
+                    </div>
+                </div>
+
+                <div className="feed__Main">
+                    <img className="body"></img>
+                </div>
+
+                <div className="feed__Bottom">
+                    <img className="resBtn" src=""></img>
+                    <img className="resBtn" src=""></img>
+                </div>
+
+            </div>
+
+        </main>
+   )
 }
 
 export default Community
