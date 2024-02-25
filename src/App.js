5f19c620fee0109f8253338e6223ce55768a4369
Aldee <aldee.cariaso@gmail.com>
2024-02-25 13:09:12 -0700

    removed Journala and Header in App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Journal from './components/Journal/Journal';
 function App() {
   return (
     <div className="App">
-      {/* <Header></Header> */}
-      <Journal></Journal>
+      
+     
       <HomeHub></HomeHub>
       
     </div>
