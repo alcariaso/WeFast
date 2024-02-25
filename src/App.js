6f387c30b02409942f81519dda38a490c8fb6938
Aldee <aldee.cariaso@gmail.com>
2024-02-25 13:27:15 -0700

    removed unnecessary import

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import Header from '../src/components/Header/Header'
+
 import './App.css';
 import HomeHub from './components/HomeHub/HomeHub';
-import Recipes from './components/Recipe/Recipe';
-import NutritionFood from './components/NutritionFood/NutritionFood';
-import RecipeCards from './components/RecipeCards/RecipeCard';
+
 
 function App() {
   return (
     <div className="App">
       
-      <NutritionFood></NutritionFood>
-      <h1>Food Recipe Choices</h1>
-  <Recipes></Recipes>
-  
+     
+      <HomeHub></HomeHub>
+      
     </div>
   );
 }
